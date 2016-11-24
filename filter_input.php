399110ce4fba2c4058c03bcd17a11fbcddf5f155
aleckyann@gmail.com <aleckyann@gmail.com>
2016-11-24 14:41:25 -0200

    FILTRANDO INPUTS

--- filter_input.php
+++ filter_input.php
@@ -1,26 +1,53 @@
 <?php 
 
-function filter_string($string){
-  $result = strip_tags(filter_var($string, FILTER_SANITIZE_STRING));
+function filter_string($string, $method){
+  if($method == 'GET' || $method == 'get' || $method == 'g'){
+    $result = strip_tags(filter_input(INPUT_GET, $string, FILTER_SANITIZE_STRING));
+  }
+  if($method == 'POST' || $method == 'post' || $method == 'p'){
+    $result = strip_tags(filter_input(INPUT_POST, $string, FILTER_SANITIZE_STRING));    
+  }
   return $result;
 }
 
-function filter_int($int){
-  $result = strip_tags(filter_var($int, FILTER_VALIDATE_INT));
+function filter_int($int, $method){
+  if($method == 'GET' || $method == 'get' || $method == 'g'){
+    $result = strip_tags(filter_input(INPUT_GET, $int, FILTER_VALIDATE_INT));
+  }
+  if($method == 'POST' || $method == 'post' || $method == 'p'){
+    $result = strip_tags(filter_input(INPUT_POST, $int, FILTER_VALIDATE_INT));    
+  }
   return $result;
 }
 
-function filter_boolean($boolean){
-  $result = strip_tags(filter_var($boolean, FILTER_VALIDATE_BOOLEAN));
+function filter_boolean($boolean, $method){
+  if($method == 'GET' || $method == 'get' || $method == 'g'){
+    $result = strip_tags(filter_input(INPUT_GET, $boolean, FILTER_VALIDATE_BOOLEAN));
+  }
+  if($method == 'POST' || $method == 'post' || $method == 'p'){
+    $result = strip_tags(filter_input(INPUT_POST, $boolean, FILTER_VALIDATE_BOOLEAN));    
+  }
   return $result;
 }
 
-function filter_float($float){
-  $result = strip_tags(filter_var($float, FILTER_VALIDATE_FLOAT));
+function filter_float($float, $method){
+  if($method == 'GET' || $method == 'get' || $method == 'g'){
+    $result = strip_tags(filter_input(INPUT_GET, $float, FILTER_VALIDATE_FLOAT));
+  }
+  if($method == 'POST' || $method == 'post' || $method == 'p'){
+    $result = strip_tags(filter_input(INPUT_POST, $float, FILTER_VALIDATE_FLOAT));    
+  }
   return $result;
 }
 
-function filter_email($email){
-  $result = strip_tags(filter_var($email, FILTER_VALIDATE_EMAIL));
+function filter_email($email, $method){
+  if($method == 'GET' || $method == 'get' || $method == 'g'){
+    $result = strip_tags(filter_input(INPUT_GET, $email, FILTER_VALIDATE_EMAIL));
+  }
+  if($method == 'POST' || $method == 'post' || $method == 'p'){
+    $result = strip_tags(filter_input(INPUT_POST, $email, FILTER_VALIDATE_EMAIL));    
+  }
   return $result;
 }
+
+?>
