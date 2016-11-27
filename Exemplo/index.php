266c3721fecf0042898b3d82c9b94cad93fa983b
aleckyann@gmail.com <aleckyann@gmail.com>
2016-11-27 00:24:21 -0200

    Update to v1.0.0

--- Exemplo/index.php
+++ Exemplo/index.php
@@ -1,11 +1,11 @@
 <?php
 require_once('../Filter.php');
 
-$data['id'] = Filter::int('usuario_id', 'p');
-$data['nome'] = Filter::string('usuario_nome', 'p');
-$data['email'] = Filter::email('usuario_email', 'p');
-$data['money'] = Filter::float('usuario_money', 'p');
-$data['ativo'] = Filter::boolean('usuario_ativo', 'p');
+$data['id'] = Filter::postInt('usuario_id');
+$data['nome'] = Filter::postString('usuario_nome');
+$data['email'] = Filter::postEmail('usuario_email');
+$data['money'] = Filter::postFloat('usuario_money');
+$data['ativo'] = Filter::postBoolean('usuario_ativo');
 
 echo('<pre>');
 print_r($data);
